0164e151ec5d986fcb674ac30e6bf54438a8c514
kwekumorgan <morganmiguel.30@gmail.com>
2025-12-05 17:40:30 +0000

    Added the vertical rectangle to HomeCard component

--- src/components/HomeCard.jsx
+++ src/components/HomeCard.jsx
@@ -1,15 +1,37 @@
 import "./HomeCard.css";
 import doctorImage from "../assets/womanDoctor.png";
+import VerticalRectangle from "../components/VerticalRectangle.jsx";
 
 function Homecard() {
   return (
     <div className="homeCard">
       <div className="homeCardText">
-        <p>Welcome back, Samira</p>
+        <p
+          style={{ fontSize: "17px", color: "#aca7a7ff", marginBottom: "50px" }}
+        >
+          Welcome Back, Samira
+        </p>
+
+        <div style={{ marginBottom: "110px", marginLeft: "30px" }}>
+          <p>
+            How do you feel today?Book an appointment with us and
+            <br />
+            get yourself checked.
+          </p>
+
+          <p style={{ padding: "0px", margin: "0px", fontWeight: "bold" }}>
+            Schedule an Appointment
+          </p>
+        </div>
+
+        <p style={{fontWeight:'600'}}>
+          Departments <span style={{ color: "white ", fontSize:'25px', marginLeft:'75px',fontWeight:'520' }}> Schedule</span>
+        </p>
       </div>
       <div className="imageContainer">
         <img src={doctorImage} alt="doctor image" className="doctorImage" />
       </div>
+      <VerticalRectangle/>
     </div>
   );
 }
