998169996f8304fdf35d3fe751ef487a1ba4757f
kwekumorgan <morganmiguel.30@gmail.com>
2026-01-06 15:34:21 +0000

    modify the route file

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -1,35 +1,10 @@
 import express from "express";
-import User from "../models/User.js";
+import { loginController } from "../controllers/loginController.js";
+import { registerController } from "../controllers/registerController.js"; 
 
-const router = express.Router();
+const authRoute = express.Router();
 
-router.post("/login", async (req, res) => {
-  try {
-    const { userId, password } = req.body;
+authRoute.post("/login", loginController);
+authRoute.post("/register", registerController); 
 
-    if (!userId || !password) {
-      return res.status(400).json({ message: "All fields are required" });
-    }
-
-    const user = await User.findOne({ userId });
-
-    if (!user) {
-      return res.status(401).json({ message: "Invalid ID or password" });
-    }
-
-    if (user.password !== password) {
-      return res.status(401).json({ message: "Invalid ID or password" });
-    }
-
-    res.status(200).json({
-      message: "Login successful",
-      user: {
-        id: user._id,
-        userId: user.userId
-      }
-    });
-  } catch (error) {
-    console.error(error);
-    res.status(500).json({ message: "Server error" });
-  }
-});
+export default authRoute;
