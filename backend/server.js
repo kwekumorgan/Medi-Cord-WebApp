cafad7172485b13e48701f3ed67ee7879a25f53e
kwekumorgan <morganmiguel.30@gmail.com>
2026-01-06 15:28:25 +0000

    modify the server file

--- backend/server.js
+++ backend/server.js
@@ -1,28 +1,26 @@
-import express from 'express';
-import dotenv from 'dotenv';
+import express from "express";
+import dotenv from "dotenv";
+import connectDB from "./config/db.js";
+import authRoutes from "./routes/authRoutes.js";
+import cors from "cors";
 
 dotenv.config();
-
+connectDB();
 
 const app = express();
 
-
-const PORT =process.env.PORT || 3000;
-
+const PORT = process.env.PORT || 3000;
 
 // Middleware to parse incoming JSON payloads
-app.use(express.json())
+app.use(express.json());
+app.use(cors());
+app.use("/api/auth", authRoutes);
 
 app.get("/", (req, res) => {
-  res.send("backend is running succesfully");
+  res.send("Backend is now running succesfully.");
 });
 
-
-
 // Start server and listen on defined port
-app.listen(PORT,()=>{
-    console.log(`server is running successfully on ${PORT}`)
-    
-})
-
- 
+app.listen(PORT, () => {
+  console.log(`Server is running successfully on ${PORT}`);
+});
